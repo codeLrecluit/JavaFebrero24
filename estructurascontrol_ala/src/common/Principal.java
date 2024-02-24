d4a9f07bf98a024a45d76e9f2fc3731b14eeb5a5
AsusLex <alangarciachampions537@gmail.com>
2024-02-23 18:47:09 -0600

    proyectos_ala

---
+++ estructurascontrol_ala/src/common/Principal.java
@@ -0,0 +1,68 @@
+package common;
+
+public class Principal {
+
+	
+	public static void main(String[] args) {
+		
+		//IF, IF-ELSE, SHWITCH, se designa ala persona
+	    //AND OR NOT
+		
+		//w3schools
+		
+		int x= 40;
+		int z=93;
+		
+//		System.out.println(x>20);
+//		
+//		if(x>20){
+//		
+//		System.out.println("OK");
+//	
+//	    }else {
+//		System.out.println("NO");
+//	    }
+		
+		//OR
+//		if(x<20  z < 150 && z <190){
+//			System.out.println("OK");
+//		}else {
+//			System.out.println("NO");
+//		}
+		
+		String m="PUEBLA";
+		
+		System.out.println(m.length());
+		System.out.println(m.length()>10);
+		
+		//NOT
+		if(!(m.length()>10)){
+			System.out.println("OK");
+		}
+		
+		//Generar el diagrama de flujo de cada uno de estos 3 ejercicios
+		
+		
+//-------------------------------------------------------------
+		
+		//Switch
+		
+		int seleccion=0;
+		
+		if(seleccion==1) {
+			System.out.println("Consulta de saldo");
+		}else if(seleccion==2) {
+			System.out.println("Retiro");
+		}else if(seleccion==3) {
+		    System.out.println("Deposito");
+		}else if(seleccion==4) {
+			System.out.println("Recarga");
+		}
+		
+		
+		
+		
+		
+		
+				
+}
---
+++ estructurascontrol_ala/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author AlanGarcia
+ *
+ */
+module estructurascontrol {
+}
